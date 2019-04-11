49c5323c5cdd3cc167225fc2f85b19d86e2f120c
tanmaniac <tanmaybangalore@gmail.com>
2019-04-10 20:50:31 -0400

    Increment an arbitrary-precision integer

--- epi_judge_cpp/int_as_array_increment.cc
+++ epi_judge_cpp/int_as_array_increment.cc
@@ -2,8 +2,21 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> PlusOne(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+  int carry = 1;
+  int idx = A.size() - 1;
+  while (carry != 0 && idx >= 0) {
+      int digit = A[idx] + carry;
+      carry = digit / 10;
+      A[idx] = digit % 10;
+      idx--;
+  }
+  // handle cases where we add another digit, e.g. 999 -> 1000
+  if (carry != 0) {
+      A.push_back(0);
+      A[0] = 1;
+  }
+
+  return A;
 }
 
 int main(int argc, char* argv[]) {
