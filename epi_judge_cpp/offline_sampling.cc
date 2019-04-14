7886f6d6bfdbba770d50e3a669c3204bc13e32b2
tanmaniac <tanmaybangalore@gmail.com>
2019-04-14 18:51:21 -0400

    Sample offline data

--- epi_judge_cpp/offline_sampling.cc
+++ epi_judge_cpp/offline_sampling.cc
@@ -2,14 +2,22 @@
 #include <functional>
 #include <iterator>
 #include <vector>
+#include <random>
 #include "test_framework/generic_test.h"
 #include "test_framework/random_sequence_checker.h"
 #include "test_framework/timed_executor.h"
 using std::bind;
 using std::vector;
 void RandomSampling(int k, vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return;
+  auto& A = *A_ptr;
+  std::random_device r;
+  std::default_random_engine engine(r());
+
+  for (int i = 0; i < k; i++) {
+      std::uniform_int_distribution<int> dist(i, A.size() - 1);
+      std::swap(A[i], A[dist(engine)]);
+  }
+  A = std::vector<int>(A.begin(), A.begin() + k);
 }
 bool RandomSamplingRunner(TimedExecutor& executor, int k, vector<int> A) {
   vector<vector<int>> results;
