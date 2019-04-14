7a8be853196508da0b6b1b88d3d74df7c19473e8
tanmaniac <tanmaybangalore@gmail.com>
2019-04-14 17:48:50 -0400

    Enumerate primes up to n

--- epi_judge_cpp/prime_sieve.cc
+++ epi_judge_cpp/prime_sieve.cc
@@ -3,8 +3,22 @@
 using std::vector;
 // Given n, return all primes up to and including n.
 vector<int> GeneratePrimes(int n) {
-  // TODO - you fill in here.
-  return {};
+  // Set all non-primes first
+  std::vector<bool> isPrime(n, true);
+  std::vector<int> primes;
+  for (int i = 0; i <= n; i++) {
+      if (i <= 1) {
+          isPrime[i] = false;
+      }
+      if (isPrime[i]) {
+          primes.push_back(i);
+          // Set all multiples to false
+          for (int mult = 2; mult * i <= n; mult++) {
+              isPrime[mult * i] = false;
+          }
+      }
+  }
+  return primes;
 }
 
 int main(int argc, char* argv[]) {
